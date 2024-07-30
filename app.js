1e3d531fa53657d1fbcecf3c9ce20e5ebac8840f
Can Bozyiğit <canbozyigit@Can-MacBook-Air.local>
2024-07-30 10:26:11 +0300

    vue

---
+++ app.js
@@ -0,0 +1,72 @@
+const {createApp}=Vue
+    var one =createApp({
+    data(){
+        return{
+            name:"can",
+            a:2,
+            b:3,
+            website:"http://youtube.com",
+            age:25,
+            karakterler:["Adnan","Bihter","matmazel"],
+            ozellikler:[
+                {name:"Adnan", age:25},
+                {name:"Bihter", age:26},
+                {name:"Matmazel", age:37}
+        
+
+            ],
+            title:"1. ınstance başlığı"
+
+        }
+    },
+    methods:{
+        hesapla: function(){
+            return this.a+this.b;
+        },
+
+        isim: function(name){
+            return  "ismi: "+this.name
+        },
+        ekle: function(){
+            this.age++;
+        },
+        cikar: function(){
+            this.age--;
+
+        }
+
+    },
+    computed:{
+        aYaEkle: function(){
+            console.log("a kontrol edildi")
+            return this.a+this.age;
+        },
+        bYeEkle: function(){
+            console.log("b kontrol edildi")
+            return this.b+this.age;
+        },
+        greet :function(){
+            return "App 1'in başlığı";
+        }
+
+
+    }
+}).mount("#app-one") ;
+
+const two = Vue.createApp({
+    data() {
+        return {
+            title: "2. title başlığı"
+        };
+    },
+    methods: {
+        changetitle() {
+            this.title = "1. instance başlığı değişti";
+        }
+    },
+    computed: {
+        greet() {
+            return "App 2'nin başlığı";
+        }
+    }
+}).mount("#app-two");
---
+++ app2.js
@@ -0,0 +1,45 @@
+const { createApp } = Vue;
+
+const one = createApp({
+    data() {
+        return {};
+    }
+});
+
+one.component("eleman", {
+    template: '<p>Ben eleman tagının içerisiyim. Benim adım {{ name }}</p><button v-on:click="changeName">isim değiştir</button>',
+    data() {
+        return {
+            name: "adnan ziyagil"
+        };
+    },
+    methods: {
+        changeName() {
+            this.name = "bihter ziyagil";
+        }
+    }
+});
+
+one.mount("#app-one");
+
+const two = createApp({
+    data() {
+        return {};
+    }
+});
+
+two.component("eleman", {
+    template: '<p>Ben eleman tagının içerisiyim. Benim adım {{ name }}</p><button v-on:click="changeName">isim değiştir</button>',
+    data() {
+        return {
+            name: "adnan ziyagil"
+        };
+    },
+    methods: {
+        changeName() {
+            this.name = "bihter ziyagil";
+        }
+    }
+});
+
+two.mount("#app-two");
